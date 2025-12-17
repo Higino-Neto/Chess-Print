4cfa6288f0c31db9c6336677c82576f025d348cb
Higino P. C. Neto <143333958+Higino-Neto@users.noreply.github.com>
2025-12-17 20:59:42 -0300

    Dockerfile reconfigured

--- application/entrypoint.sh
+++ application/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-echo "Waiting MySQL..."
-until nc -z mysql 3306; do
-  sleep 1
+
+DB_HOST_NAME=${DB_HOST:-localhost}
+
+echo "Waiting for database at $DB_HOST_NAME:3306..."
+
+while ! nc -z $DB_HOST_NAME 3306; do
+  echo "Database is unavailable - sleeping"
+  sleep 2
 done
-echo "MySQL Ready!"
+
+echo "Database Ready!"
 
 echo "Sincronizing tables..."
 node database/sync.js
 
-echo "Starting Next.js..."
-npm run dev -- -H 0.0.0.0
+echo "Starting Next.js in Production mode..."
+npm start
