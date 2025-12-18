1e679b1216c1f77738a32195dcedd43730b2aaf1
Higino P. C. Neto <143333958+Higino-Neto@users.noreply.github.com>
2025-12-17 22:41:19 -0300

    Add PostgreSQL support to MySQL database config

--- application/database/mysql.js
+++ application/database/mysql.js
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
 import 'dotenv/config';
+import pg from "pg";
 
 const databaseUrl = process.env.DATABASE_URL;
 
 const mysql = new Sequelize(databaseUrl, {
   dialect: "postgres",
+  dialectModule: pg,
 
   dialectOptions: {
     ssl: {
