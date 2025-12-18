1f55a3813912bb629c295fc66ecdedef176d8540
Tawl <tawl.tack@gmail.com>
2025-12-17 22:06:47 -0300

    Test Release

--- application/src/app/usuarios/new/page.jsx
+++ application/src/app/usuarios/new/page.jsx
@@ -1,13 +1,18 @@
 import createUsuario from "@/app/actions/usuarios";
 
 export default async function FormUsuario() {
+
+
     return (
-        <form action={createUsuario}>
-            <h2>Insert Usuario</h2>
-            <input name="nome" placeholder="Name" /> <br />
-            <input name="email" placeholder="Email" /> <br />
-            <input type="password" name="senha" placeholder="Password" /> <br />
-            <button type="submit">Sign Up Usuario</button>
-        </form>
+        <div className="flex items-center justify-center">
+            <form action={createUsuario} className="rounded-lg p-5 flex flex-col bg-[#414156] text-white">
+                <h2>Insert Usuario</h2>
+                <input name="nome" placeholder="Name" className="bg-[#34344C] p-2 mt-2 w-60"/> <br />
+                <input name="email" placeholder="Email" className="bg-[#34344C] p-2"/> <br />
+                <input type="password" name="senha" placeholder="Password" className="bg-[#34344C] p-2"/> <br />
+                <button type="submit" className="ml-2 mr-2 h-8 px-2 py-1 cursor-pointer text-[#191927] bg-[#8e72ee] rounded-md shadow-md
+                            transform transition-all duration-300 hover:scale-103 hover:shadow-[#191927]">Sign Up Usuario</button>
+            </form>
+        </div>
     );
 }
--- application/src/app/usuarios/page.jsx
+++ application/src/app/usuarios/page.jsx
@@ -1,6 +1,18 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { Usuario } from "../../../database/models/tables";
+
+async function deleteUsuario(formData) {
+    'use server';
+
+    const id = formData.get('id');
+
+    const usuario = await Usuario.findByPk(id);
+    await usuario.destroy();
+
+    redirect('/usuarios')
+}
 
 export default async function showUsuarios() {
 
@@ -10,8 +22,13 @@ export default async function showUsuarios() {
         <div>
             <h1 className="pb-6 text-4xl font-bold bg-clip-text text-transparent 
                bg-gradient-to-t from-[#00d7d2] to-[#8e72ee] text-center mt-8">
-                List of Usuarios
+                Lista de Usuarios
             </h1>
+
+            <form action={'/usuarios/new'}>
+                <button type="submit" className="m-1 h-8 px-6 py-1 cursor-pointer text-[#191927] bg-[#8e72ee] rounded-md shadow-md
+                            transform transition-all duration-300 hover:scale-103 hover:shadow-[#191927]">Create new</button>
+            </form>
             <div className="border-collapse border border-gray-800 rounded-lg overflow-hidden shadow-lg">
                 <table className="w-full">
                     <thead className="bg-[#414156] border-b border-gray-800">
@@ -20,15 +37,32 @@ export default async function showUsuarios() {
                             <th className="p-3 text-sm font-semibold tracking-wide text-left text-gray-200">Name</th>
                             <th className="p-3 text-sm font-semibold tracking-wide text-left text-gray-200">Email</th>
                             <th className="p-3 text-sm font-semibold tracking-wide text-left text-gray-200">Password</th>
+                            <th className="p-3 text-sm font-semibold tracking-wide text-left text-gray-200">Edit</th>
+                            <th className="p-3 text-sm font-semibold tracking-wide text-left text-gray-200">Delete</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody className="">
                         {usuarios.map(usuario => (
                             <tr className="odd:bg-[#2B2B3F] even:bg-[#32324F]" key={usuario.id}>
                                 <td className="p-3 text-sm text-gray-200">{usuario.id}</td>
                                 <td className="p-3 text-sm text-gray-200">{usuario.nome}</td>
                                 <td className="p-3 text-sm text-gray-200">{usuario.email}</td>
                                 <td className="p-3 text-sm text-gray-200">{usuario.senha}</td>
+                                <td className="">
+                                    <form action={'/usuarios/edit'}>
+                                        <input type="hidden" name="id" defaultValue={usuario.id} />
+                                        <button className=" h-8 px-6  py-1 cursor-pointer text-[#191927] bg-[#8e72ee] rounded-md shadow-md
+                            transform transition-all duration-300 hover:scale-103 hover:shadow-[#191927]">Edit</button>
+                                    </form>
+
+                                </td>
+                                <td>
+                                    <form action={deleteUsuario}>
+                                        <input type="hidden" name="id" defaultValue={usuario.id} />
+                                        <button className="h-8 px-6 py-1 cursor-pointer text-[#191927] bg-[#8e72ee] rounded-md shadow-md
+                            transform transition-all duration-300 hover:scale-103 hover:shadow-[#191927]">Delete</button>
+                                    </form>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
